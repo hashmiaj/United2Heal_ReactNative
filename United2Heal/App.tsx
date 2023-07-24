1a934d17fb2a320500559c4dea17b4c4b87b8aa1
Abdullah Hashmi <abdullah.hashmi21@gmail.com>
2023-07-23 22:52:50 -0700

    Cleaning up App.tsx

--- United2Heal/App.tsx
+++ United2Heal/App.tsx
@@ -3,20 +3,6 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
 import Welcome from './WelcomePage';
 
 function App(): JSX.Element {
