d9bbf91c0f31c4ff657667920dbcec58857d50aa
Elsh22 <aelshowaya@gmail.com>
2023-08-02 12:08:47 -0400

    Update version
    Updated version

--- United2Heal/AdminScreen.js
+++ United2Heal/AdminScreen.js
@@ -1,25 +1,55 @@
 // AdminScreen.js
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const AdminScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome to the Admin Screen</Text>
-        </View>
-    );
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Handle the login logic here
+    console.log('Username: ', username);
+    console.log('Password: ', password);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Username:</Text>
+      <TextInput 
+        style={styles.input} 
+        value={username} 
+        onChangeText={(text) => setUsername(text)} 
+      />
+      <Text style={styles.label}>Password:</Text>
+      <TextInput 
+        style={styles.input} 
+        value={password} 
+        onChangeText={(text) => setPassword(text)} 
+        secureTextEntry
+      />
+      <Button title="Submit" onPress={handleLogin} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 20,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
 });
 
 export default AdminScreen;
--- United2Heal/VolunteerScreen.js
+++ United2Heal/VolunteerScreen.js
@@ -1,25 +1,55 @@
 // VolunteerScreen.js
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const VolunteerScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Welcome to the Volunteer Screen</Text>
-        </View>
-    );
+  const [fullName, setFullName] = useState('');
+  const [sessionGroup, setSessionGroup] = useState('');
+
+  const handleSubmit = () => {
+    // Handle the submit logic here
+    console.log('Full Name: ', fullName);
+    console.log('Session Group: ', sessionGroup);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Full Name:</Text>
+      <TextInput 
+        style={styles.input} 
+        value={fullName} 
+        onChangeText={(text) => setFullName(text)} 
+      />
+      <Text style={styles.label}>Session Group:</Text>
+      <TextInput 
+        style={styles.input} 
+        value={sessionGroup} 
+        onChangeText={(text) => setSessionGroup(text)} 
+      />
+      <Button title="Submit" onPress={handleSubmit} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        fontSize: 20,
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 10,
+  },
 });
 
 export default VolunteerScreen;
+
