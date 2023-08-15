107a779c0ba46c7baf8e7a70184dd4288467eb26
YabbyWondwosen <139565334+YabbyWondwosen@users.noreply.github.com>
2023-08-15 10:50:09 -0400

    Added volunteer name & group number to home tab

--- United2Heal/HomeTab.js
+++ United2Heal/HomeTab.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import U2HConfigNode from './U2HConfigNode';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const HomeTab = () => {
+  const volunteerName = U2HConfigNode.getVolunteerName();
+  const groupName = U2HConfigNode.getGroupName();
+
   return (
     <View style={styles.container}>
-      <Text>This is the Home tab.</Text>
-    </View>
+      <Text style={styles.title}>Welcome {volunteerName}</Text>
+      <Text style= {styles.description}>Group: {groupName}</Text>
+    </View> 
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ffffff',
   },
+  title: {
+    fontSize: 32,
+    color: '#0000FF', 
+    marginTop: 50,
+    fontWeight: 'bold'
+  },
+  description: {
+    fontSize: 20, 
+    marginTop: 20,
+    fontWeight: 'bold'
+  }
 });
 
 export default HomeTab;
