37703e786a292c21bf3078b19a2d305d46c43eb1
YabbyWondwosen <139565334+YabbyWondwosen@users.noreply.github.com>
2023-08-22 15:32:21 -0400

    Added submitted items and borders for the home tab

--- United2Heal/HomeTab.js
+++ United2Heal/HomeTab.js
@@ -6,11 +6,18 @@ import { Colors } from 'react-native/Libraries/NewAppScreen';
 const HomeTab = () => {
   const volunteerName = U2HConfigNode.getVolunteerName();
   const groupName = U2HConfigNode.getGroupName();
+  const itemCount = 50;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome {volunteerName}</Text>
-      <Text style= {styles.description}>Group: {groupName}</Text>
+      <View style={styles.outline}>
+        <Text style={styles.title}>Welcome {volunteerName}!</Text>
+        <Text style= {styles.description}>Group: {groupName}</Text>
+      </View>
+      <View style={styles.outline}>
+       <Text style= {styles.submittedItems}>Submitted Items</Text>
+       <Text style= {styles.itemCount}>{itemCount}</Text>
+      </View>
     </View> 
   );
 };
@@ -21,16 +28,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffffff',
   },
+  outline: {
+    borderColor: 'black',
+    borderWidth: 1,
+    marginTop: 32,
+    alignItems: 'center',
+    padding: 16,
+    width: '85%',
+  },
   title: {
     fontSize: 32,
     color: '#0000FF', 
-    marginTop: 50,
     fontWeight: 'bold'
   },
   description: {
     fontSize: 20, 
     marginTop: 20,
     fontWeight: 'bold'
+  },
+  submittedItems: {
+    fontSize: 28,
+  },
+  itemCount: {
+    fontSize: 60,
+    marginTop: 20,
   }
 });
 
